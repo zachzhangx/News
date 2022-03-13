0485017a2f1a8d390e9eb59a6de52c357ad76a12
zachzhangx <101433206+zachzhangx@users.noreply.github.com>
2022-03-13 01:02:11 -0800

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Zhang Xu on 2022/3/13.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
